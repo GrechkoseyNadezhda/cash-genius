7e12fd4a468041962bdf7e44ab9791320aab7e44
NikoCheee <nikita3ch@gmail.com>
2023-05-02 15:10:27 +0300

    Add main page view for url test

--- site_app/views.py
+++ site_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def main_page(request):
+    return Response()
