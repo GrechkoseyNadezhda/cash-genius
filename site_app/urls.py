97091e1d2d955a4fab06ca971d762c4dace95183
NikoCheee <nikita3ch@gmail.com>
2023-05-02 18:47:44 +0300

    Add endpoints and views for them

--- site_app/urls.py
+++ site_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('about/', views.main_page, name='about'),
+    path('financial_guide/', views.main_page, name='fin_guide'),  # усі статті
+    path('<slug:category>', views.main_page, name='category'),  # статті певної категорії
+    path('article/<int:pk>', views.main_page, name='article'),
+    path('article/', views.main_page, name='article_create'),  # форма щоб запостити статтю
 ]
--- site_app/views.py
+++ site_app/views.py
@@ -2,6 +2,33 @@ from django.shortcuts import render
 from rest_framework.views import Response
 from rest_framework.decorators import api_view
 
+
 @api_view(['GET'])
 def main_page(request):
     return Response()
+
+
+@api_view(['GET'])
+def about(request):
+    return Response()
+
+
+@api_view(['GET'])
+def financial_guide(request):
+    return Response()
+
+
+@api_view(['GET'])
+def category(request):
+    return Response()
+
+
+@api_view(['GET'])
+def article(request, pk):
+    return Response()
+
+
+@api_view(['GET', 'POST'])
+def article_create(request):
+    return Response()
+
