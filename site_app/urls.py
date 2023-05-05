57321b2716c9e9bd81190b34d1fa83955fd125f7
NikoCheee <nikita3ch@gmail.com>
2023-05-05 15:39:21 +0300

    Fix url-roots with correct views

--- site_app/urls.py
+++ site_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('about/', views.main_page, name='about'),
-    path('financial_guide/', views.main_page, name='fin_guide'),  # усі статті
-    path('<slug:category>', views.main_page, name='category'),  # статті певної категорії
-    path('article/<int:pk>', views.main_page, name='article'),
-    path('article/', views.main_page, name='article_create'),  # форма щоб запостити статтю
+    path('about/', views.about, name='about'),
+    path('financial_guide/', views.financial_guide, name='fin_guide'),  # усі статті
+    path('<slug:category>', views.category, name='category'),  # статті певної категорії
+    path('article/<int:pk>', views.article, name='article'),
+    # path('article/', views.main_page, name='article_create'),  # форма щоб запостити статтю
 ]
