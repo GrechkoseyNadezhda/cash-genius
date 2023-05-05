1fe54055a182bafe87cce79eadd5aabc5b5ec2f4
NikoCheee <nikita3ch@gmail.com>
2023-05-05 16:38:09 +0300

    Fix category slug conflict

--- site_app/urls.py
+++ site_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('about/', views.about, name='about'),
     path('financial_guide/', views.financial_guide, name='fin_guide'),  # усі статті
-    path('<slug:category>', views.category, name='category'),  # статті певної категорії
+    path('<slug:category_slug>/', views.category, name='category'),  # статті певної категорії
     path('article/<int:pk>', views.article, name='article'),
     # path('article/', views.main_page, name='article_create'),  # форма щоб запостити статтю
 ]
