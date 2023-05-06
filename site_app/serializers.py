efeb108265dacdddd2f053ff017f2b31c704a24c
NikoCheee <nikita3ch@gmail.com>
2023-05-07 00:14:47 +0300

    Add EnglishArticle model serializer for nested serialization

--- site_app/serializers.py
+++ site_app/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, EnglishArticle
+
+
+class EnglishArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EnglishArticle
+        fields = ['title', 'content']
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     category = serializers.StringRelatedField()
     author = serializers.StringRelatedField()
-    english_version = serializers.StringRelatedField()
+    english_version = EnglishArticleSerializer(read_only=True)
     tags = serializers.StringRelatedField(many=True)
 
     class Meta:
