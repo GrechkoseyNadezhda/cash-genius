e0564daaf90d295c3ea3942f16d03a8b784fbecf
NikoCheee <nikita3ch@gmail.com>
2023-05-09 18:21:25 +0300

    Add url settings for media rootes

--- site_app/urls.py
+++ site_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     path('<slug:category_slug>/', views.category, name='category'),  # статті певної категорії
     path('article/<int:pk>', views.article, name='article'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
