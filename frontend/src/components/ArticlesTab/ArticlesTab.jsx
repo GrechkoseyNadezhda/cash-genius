5a66a25901901b95602202144f9fd28864a894c9
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-05-10 22:16:34 +0300

    refactor

--- frontend/src/components/ArticlesTab/ArticlesTab.jsx
+++ frontend/src/components/ArticlesTab/ArticlesTab.jsx
@@ -4,23 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllArticles } from "../../redux/operations";
 import { selectGlobal } from "../../redux/selectors";
 import { loadFromDB } from "../../loadFromDB";
-import { Article } from "../Article/Article";
 import { Categories } from "../Categories/Categories";
 import { ArticlesList } from "../ArticlesList/ArticlesList";
 
 export const ArticlesTab = () => {
   const { t } = useTranslation(["articles"]);
-  const { lang, error, pending } = useSelector(selectGlobal);
+  const { error, pending } = useSelector(selectGlobal);
   const [articles, setArticles] = useState([]);
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
   const dispatch = useDispatch();
   const loader = useMemo(() => {
-    return loadFromDB(getAllArticles, setArticles, ["data"], dispatch, page);
-  }, [dispatch, page]);
+    return loadFromDB(getAllArticles, setArticles, ["data"], dispatch);
+  }, [dispatch]);
 
   useEffect(() => {
     return loader();
-  }, [loader, page]);
+  }, [loader]);
 
   return (
     <div>
