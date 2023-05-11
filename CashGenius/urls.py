11752a757b72e1bb74f63ef8eae34a33e13ba46c
NikoCheee <nikita3ch@gmail.com>
2023-05-11 18:30:51 +0300

    Rebase url-roots media settings from app to project level

--- CashGenius/urls.py
+++ CashGenius/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns += i18n_patterns(
     path('', include('site_app.urls')),
     prefix_default_language=False,
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
