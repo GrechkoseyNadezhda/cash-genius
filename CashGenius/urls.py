a0dbc17f7ec3317e85266bf42d4552e3d4ceb87e
NikoCheee <nikita3ch@gmail.com>
2023-05-11 17:32:46 +0300

    Add import for internationalization function

--- CashGenius/urls.py
+++ CashGenius/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 
 urlpatterns = [
