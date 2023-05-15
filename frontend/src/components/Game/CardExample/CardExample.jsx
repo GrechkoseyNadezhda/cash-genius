5fe677ef1fd844aa0c1f2d79b8a287ee5ffc1693
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2023-05-15 11:44:19 +0200

    game page

---
+++ frontend/src/components/Game/CardExample/CardExample.jsx
@@ -0,0 +1,22 @@
+import css from "./CardExample.module.css";
+
+export const CardExample = ({ title, text1, text2, text3 }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.thead}>
+        <tr className={css.tr}>
+          <th className={css.th} colspan="3">
+            {title}
+          </th>
+        </tr>
+      </thead>
+      <tbody className={css.tbody}>
+        <tr className={css.tr}>
+          <td className={css.td}>{text1}</td>
+          <td className={css.td}>{text2}</td>
+          <td className={css.td}>{text3}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
