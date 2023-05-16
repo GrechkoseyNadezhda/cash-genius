e71173bdd65376684e0219ff1dd04b7604663ac7
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-05-16 22:26:16 +0300

    Update i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -19,7 +19,7 @@ i18n
     },
     ns: ["header", "game", "articles", "about", "footer"],
     backend: {
-      loadPath: "/cash-genius/assets/locales/{{ns}}/{{lng}}.json",
+      loadPath: "/Cash-Genius/assets/locales/{{ns}}/{{lng}}.json",
     },
   });
 export default i18n;
