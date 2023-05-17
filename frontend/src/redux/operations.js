24632402cf660656c8b85031128090da774bc29e
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-05-17 20:16:45 +0300

    api added

--- frontend/src/redux/operations.js
+++ frontend/src/redux/operations.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axIstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
-  // baseURL: "https://bandydan.pythonanywhere.com/api/",
+  // baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://bandydan.pythonanywhere.com/api/",
 });
 
 export async function getAllArticles(category, params) {
