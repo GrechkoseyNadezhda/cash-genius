3bd030d08a6a57c12e8ee17dce61191937b0ea94
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-05-18 13:01:43 +0300

    articles formatted

--- frontend/src/components/ArticlePreview/ArticlePreview.jsx
+++ frontend/src/components/ArticlePreview/ArticlePreview.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import css from "./ArticlePreview.module.css";
+import DOMPurify from "dompurify";
 export const ArticlePreview = ({ category, article }) => {
   const location = useLocation();
   return (
@@ -8,15 +9,15 @@ export const ArticlePreview = ({ category, article }) => {
         <img src={article.image} alt="" className={css.picture} />
         <p className={css.date}>{article.date_added}</p>
         <h3 className={css.artTitle}>{article.title}</h3>
-        <p className={css.content}>{article.content}</p>
+        <p
+          className={css.content}
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(article.content),
+          }}
+        >
+          {/* {article.content} */}
+        </p>
       </div>
     </Link>
-    // <Link to={`${post.pk}`} state={{ from: location }}>
-    //   <h4>{post.title}</h4>
-    //   <h5>{post.author}</h5>
-    //   <p>{post.content}</p>
-    //   <p>{post.date_added}</p>
-    //   <img src={`${post.image}`} alt="" />
-    // </Link>
   );
 };
