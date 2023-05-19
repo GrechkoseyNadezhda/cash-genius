7743a0fa66ca52320457b0ba4af48c360c1e6635
NikoCheee <nikita3ch@gmail.com>
2023-05-19 22:34:54 +0300

    Remove redundant api url

--- site_app/urls.py
+++ site_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('about/', views.about, name='about'),
     path('financial_guide/', views.financial_guide, name='fin_guide'),  # усі статті
     path('<slug:category_slug>/', views.category, name='category'),  # статті певної категорії
     path('article/<int:pk>', views.article, name='article'),
