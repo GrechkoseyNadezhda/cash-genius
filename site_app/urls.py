d7be390cf276c134e29651c4dbc9c312c1d242e7
NikoCheee <nikita3ch@gmail.com>
2023-05-19 22:36:15 +0300

    Fix api urls

--- site_app/urls.py
+++ site_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('financial_guide/', views.financial_guide, name='fin_guide'),  # усі статті
-    path('<slug:category_slug>/', views.category, name='category'),  # статті певної категорії
+    path('financial_guide', views.financial_guide, name='fin_guide'),  # усі статті
+    path('category/<slug:category_slug>', views.category, name='category'),  # статті певної категорії
     path('article/<int:pk>', views.article, name='article'),
 ]
 
