7dd4ca4af1fb74a55999ea300e5274a9c5390d88
NikoCheee <nikita3ch@gmail.com>
2023-05-19 23:06:20 +0300

    Add staticfiles url managing

--- CashGenius/urls.py
+++ CashGenius/urls.py
@@ -19,11 +19,14 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('site_app.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns += i18n_patterns(
     path("i18n/", include("django.conf.urls.i18n")),
