4d9997929169d815a19f00e18c397d4d392cf919
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-05-22 12:50:07 +0300

    Update i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,26 +3,28 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
+const selectedLanguage = localStorage.getItem("i18nextLng");
+
 i18n
   .use(LanguageDetector)
   .use(Backend)
   .use(initReactI18next)
   .init({
     debug: false,
+    lng: selectedLanguage || "ua",
     detection: {
       order: ["localStorage", "navigator"],
       caches: ["localStorage"],
-      lookupLocalStorage: "ua",
-      checkWhitelist: true,
     },
-    whitelist: ["ua", "en"],
+
     fallbackLng: "ua",
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
     ns: ["header", "game", "articles", "about", "footer"],
     backend: {
       loadPath: "/assets/locales/{{ns}}/{{lng}}.json",
     },
   });
+
 export default i18n;
