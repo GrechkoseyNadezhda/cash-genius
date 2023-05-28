ece12cf057be9df378bb2459475d509289aa18db
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2023-05-28 19:31:00 +0200

    Update Game.jsx

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -1,6 +1,7 @@
 import { Hero } from "./Hero/Hero";
 import { Benefits } from "./Benefits/Benefits";
 import { Rules } from "./Rules/Rules";
+
 import { Сharacters } from "./Сharacters/Сharacters";
 
 export const Game = () => {
